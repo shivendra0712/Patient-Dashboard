16ad83908c4cbbfaf68fb12d97e791c76e9bc09f
shivendra0712 <patelshivendra817@gmail.com>
2025-05-13 22:22:19 +0530

    changed url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL,
+    credentials: true, 
+}));
 app.use(express.json());
 
 // Import routes
